f4f49310c10d787dae436e1b4dd12080d482de49
Artem444311 <kudinovartem65@gmail.com>
2020-01-22 15:56:57 +0200

    add  new method go

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void print(HashMap <Integer,Cat> cats){
+        for (int i=0; i<cats.size(); i++){
+            System.out.println(cats.get(i));
+        }
     }
 }
